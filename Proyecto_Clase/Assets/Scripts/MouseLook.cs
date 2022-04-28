d6e66308bb75cbe778e0462eb1249c74114b9ff7
Rapseo <103188330+Rapseo@users.noreply.github.com>
2022-04-28 14:16:23 +0200

    intento de remodelar camara

--- Proyecto_Clase/Assets/Scripts/MouseLook.cs
+++ Proyecto_Clase/Assets/Scripts/MouseLook.cs
@@ -17,7 +17,7 @@ public class MouseLook : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        
+
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 

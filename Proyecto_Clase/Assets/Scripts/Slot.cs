b838b770ceb7e17e97ca2f096a197258a432e91c
Rapseo <103188330+Rapseo@users.noreply.github.com>
2022-05-26 18:28:36 +0200

    Intento fallido de llave 2

--- Proyecto_Clase/Assets/Scripts/Slot.cs
+++ Proyecto_Clase/Assets/Scripts/Slot.cs
@@ -21,7 +21,11 @@ public class Slot : MonoBehaviour, IPointerClickHandler
 
     public void UpdateSlot()
     {
+        Debug.Log("Esto es un sprite: "+icon);
+
         slotIconGameObject.GetComponent<Image>().sprite = icon;
+
+        Debug.Log("Este es el icono: "+ slotIconGameObject.GetComponent<Image>().sprite);
     }
 
     public void UseItem()
